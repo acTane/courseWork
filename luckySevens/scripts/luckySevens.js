d65bb17b28115c4a5905ef8e02d43d48bb27d687
mac ferguson <mr.macferguson@gmail.com>
2019-10-02 11:12:07 -0500

    day7

--- luckySevens/scripts/luckySevens.js
+++ luckySevens/scripts/luckySevens.js
@@ -21,3 +21,42 @@ function luckySevensGame(bet){
 	}
 	return [numberRolls,maxMoney,numRollsMax]
 }
+
+function clearErrors(){
+	for(var loopCounter=0; loopCounter<document.forms["diceGame"].elements.length; loopCounter++){
+		if (document.forms["diceGame"].elements[loopCounter].parentElement.className.indexOf("has-")!=-1){
+			document.forms["diceGame"].elements[loopCounter].parentElement.className="form-group";
+		}
+	}
+}
+
+function resetForm(){
+	clearErrors();
+	document.forms["diceGame"]["bet"].value="";
+	document.getElementById("results").style.display="none";
+	document.getElementById("submitButton").innerText="Play";
+	document.forms["diceGame"]["bet"].focus();
+	document.getElementById("submitButton").style.display="block";
+	document.getElementById("resetButton").style.display="none";
+}
+
+function validateItems(){
+	clearErrors();
+	var num1 = document.forms["diceGame"]["bet"].value;
+	if(num1==""||num1=="0"||isNaN(num1)){
+		alert("Your starting bet must be a number.");
+		document.forms["diceGame"]["num1"].parentElement.className="form-group has-error";
+		document.forms["diceGame"]["num1"].focus();
+		return false;
+	}
+	var game=luckySevensGame(num1);
+	document.getElementById("submitButton").style.display="none";
+	document.getElementById("resetButton").style.display="block";
+	document.getElementById("results").style.display="block";
+	document.getElementById("submitButton").innerText="Recalculate";
+	document.getElementById("startBet").innerText=Number(num1);
+	document.getElementById("totalRolls").innerText = game[0];
+	document.getElementById("maxMoney").innerText = game[1];
+    document.getElementById("MRolls").innerText = game[2];
+	return false;
+}
